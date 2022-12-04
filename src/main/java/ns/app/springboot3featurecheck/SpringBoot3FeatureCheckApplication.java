b55a34cf77a5aec2dcdcc195f96919e4de46adf9
newton <newtonsarker@gmail.com>
2022-12-04 08:47:40 +0100

    Initial commit

---
+++ src/main/java/ns/app/springboot3featurecheck/SpringBoot3FeatureCheckApplication.java
@@ -0,0 +1,13 @@
+package ns.app.springboot3featurecheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3FeatureCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3FeatureCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ns/app/springboot3featurecheck/SpringBoot3FeatureCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package ns.app.springboot3featurecheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3FeatureCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
